732ff67a8d645dc0a376b7a9a533b2c436a642ed
gblee87 <gblee87@gmail.com>
2019-01-02 17:54:47 +0900

    change setupApache24.sh - chand permission from root to dextop

--- setupApache24.sh
+++ setupApache24.sh
@@ -1,13 +1,23 @@
 #/bin/bash
 # 아파치 2.4 설치
-echo "install gcc and g++ compiler";
+export DEFAULT_LOC = `pwd`;
+echo $DEFAULT_LOC;
+echo "==============================================================";
+echo "==============================================================";
+echo "================install gcc and g++ compiler==================";
+echo "==============================================================";
+echo "==============================================================";
 apt-get install -y gcc;
 apt-get install -y g++;
 apt install -y libexpat1-dev;
 apt-get install iy pcre-devel;
 echo "done."
 
-echo "install apr";
+echo "==============================================================";
+echo "==============================================================";
+echo "==========================install apr=========================";
+echo "==============================================================";
+echo "==============================================================";
 wget http://mirror.navercorp.com/apache//apr/apr-1.6.5.tar.gz;
 tar zxvf ./apr-1.6.5.tar.gz;
 cd apr-1.6.5;
@@ -16,7 +26,11 @@ make && make install;
 cd ..;
 echo "done";
 
-echo "install apr-util";
+echo "==============================================================";
+echo "==============================================================";
+echo "=====================install apr-util=========================";
+echo "==============================================================";
+echo "==============================================================";
 wget http://mirror.navercorp.com/apache//apr/apr-util-1.6.1.tar.gz;
 tar zxvf ./apr-util-1.6.1.tar.gz;
 cd apr-util-1.6.1;
@@ -25,14 +39,22 @@ make && make install;
 cd ..;
 echo "done.";
 
-echo "pcre installation needs manual download";
+echo "==============================================================";
+echo "==============================================================";
+echo "=========================install pcre=========================";
+echo "==============================================================";
+echo "==============================================================";
 tar zxvf ./pcre-8.39.tar.gz;
 cd pcre-8.39;
 ./configure --prefix=/usr/local/pcre;
 make && make install;
 echo "done.";
 
-echo "install apache";
+echo "==============================================================";
+echo "==============================================================";
+echo "=======================install apache=========================";
+echo "==============================================================";
+echo "==============================================================";
 wget http://apache.tt.co.kr//httpd/httpd-2.4.37.tar.gz;
 tar zxvf ./httpd-2.4.37.tar.gz;
 mkdir -p /engn001/apache/2.4;
@@ -41,8 +63,25 @@ cd httpd-2.4.37;
 make && make install;
 echo "done."
 
-echo `pwd`;
-echo "remove install file...";
+echo "==============================================================";
+echo "==============================================================";
+echo "===================remove install files=======================";
+echo "==============================================================";
+echo "==============================================================";
 cd /engn001/installscript;
-#rm -rf ./apr*;
+rm -rf ./apr* && rm -rf ./pcre& && rm -rf httpd*;
 echo "done";
+
+echo "==============================================================";
+echo "==============================================================";
+echo "=============make instance and configuration==================";
+echo "==============================================================";
+echo "==============================================================";
+mkdir -p /engn001/apache/2.4/servers/webd-apache_80;
+cp -r /engn001/apache/2.4/conf /engn001/apache/2.4/servers/webd-apache_80;
+mkdir -p /logs/apache/2.4/logs;
+ln -s /logs/apache/2.4/logs /engn001/apache/2.4/servers/webd-apache_80/logs;
+cp $DEFAULT_LOC/*.sh /engn001/apache/2.4/servers/webd-apache_80/bin;
+chown -R dextop:dextop /engn001/apache;
+su - dextop -c '/engn001/apache/2.4/servers/webd-apache_80/bin/start.sh';
+
