395fa8461c01f3509e25a614cf0c7aaa93eb9870
gblee87 <pechando@gmail.com>
2019-01-03 16:12:16 +0900

    20190103|gblee|update setupscript

--- setupApache24.sh
+++ setupApache24.sh
@@ -81,8 +81,8 @@ mkdir -p /engn001/apache/2.4/servers/webd-apache_80;
 cp -r /engn001/apache/2.4/conf /engn001/apache/2.4/servers/webd-apache_80;
 mkdir -p /logs/apache/2.4/logs;
 ln -s /logs/apache/2.4/logs /engn001/apache/2.4/servers/webd-apache_80;
-cp ${DEFAULT_LOC}/*.sh /engn001/apache/2.4/servers/webd-apache_80;
-cp ${DEFAULT_LOC}/httpd.conf /engn001/apache/2.4/servers/webd-apache_80/conf
+cp ./*.sh /engn001/apache/2.4/servers/webd-apache_80;
+cp ./httpd.conf /engn001/apache/2.4/servers/webd-apache_80/conf
 chmod -R 755 /engn001/apache;
 chown -R dextop:dextop /engn001/apache;
 su - dextop -c '/engn001/apache/2.4/servers/webd-apache_80/start.sh';
