ba39f9613e7de520b8ebc3580fbdce0183482105
gblee87 <pechando@gmail.com>
2019-01-03 10:11:46 +0900

    test

---
+++ env.sh
@@ -0,0 +1,7 @@
+export ENGN_HOME=/engn001/apache/2.4
+export SERVER_ID=webd-apache_80
+export SERVICE_PORT=2026
+export RUN_USER=dextop
+export HTTPS_SERVICE_PORT=443
+export INSTALL_PATH=$ENGN_HOME/servers/$SERVER_ID
+export DOC_ROOT=/sorc001/dextop/applications/htdocs
