ab37de179b91c8e64e4f751c39cc55c1a5b0744a
gblee87 <pechando@gmail.com>
2019-01-03 10:40:33 +0900

    20190103|gblee|add start stop script

--- start.sh
+++ start.sh
@@ -1,1 +1,28 @@
 #/bin/bash
+
+. /engn001/apache/2.4/servers/webd-apache_80/env.sh
+
+SETUSER=${WAS_USER}
+RUNNER=`whoami`
+
+if [ ${RUNNER} != ${SETUSER} ] ;
+    then echo "Deny Access : [ ${RUNNER} ]. Not [${SETUSER}]";
+    exit 0;
+fi
+
+/engn001/apache/2.4/bin/apachectl -f /engn001/apache/2.4/servers/webd-apache_80/conf/httpd.conf -k start
+#LD_LIBRARY_PATH=${CATALINA_HOME}/LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH
+
+#LOG_HOME=${CATALINA_BASE}/logs
+#export LOG_HOME
+
+#rm -rf ${CATALINA_BASE}/temp/*
+#rm -rf ${CATALINA_BASE}/work/*
+
+#DATE=`date _%Y-%m-%d_%H-%M-%S`
+
+#mv ${LOG_HOME}/gc_${INST_NAME}.log ${LOG_HOME}/gclog/gc_${INST_NAME}.log.${DATE}
+#mv ${LOG_HOME}/${INST_NAME}.out ${LOG_HOME}/nohup/${INST_NAME}.out.${DATE}
+
+#JAVA_OPTS="${JAVA_OPTS} -D${INST_NAME} -Xmx1024m -Xm"
--- stop.sh
+++ stop.sh
@@ -1,1 +1,13 @@
 #/bin/bash
+
+. /engn001/apache/2.4/servers/webd-apache_80/env.sh
+
+SETUSER=${WAS_USER}
+RUNNER=`whoami`
+
+if [ ${RUNNER} != ${SETUSER} ] ;
+    then echo "Deny Access : [ ${RUNNER} ]. Not [${SETUSER}]";
+    exit 0;
+fi
+
+/engn001/apache/2.4/bin/apachectl -f /engn001/apache/2.4/servers/webd-apache_80/conf/httpd.conf -k stop
